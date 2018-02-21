e0f8ce6e659e30e45094529e0215e22d03c97c3c
Derek Vawdrey <flyingpiechicken@gmail.com>
2018-02-21 12:25:02 -0700

    Added Twitter API

--- src/chat/model/CTECTwitter.java
+++ src/chat/model/CTECTwitter.java
@@ -1,6 +1,24 @@
 package chat.model;
 
+import chat.controller.ChatbotController;
+
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.Twitter;
+import twitter4j.Status;
+
 public class CTECTwitter
 {
-
+	private ChatbotController appController;
+	private Twitter chatbotTwitter;
+	
+	public CTECTwitter(ChatbotController appController) {
+		this.appController = appController;
+		this.chatbotTwitter = TwitterFactory.getSingleton();
+	}
+	
+	public void sendTweet(String textToTweet) {
+		
+	}
+	
 }
