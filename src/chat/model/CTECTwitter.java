eb8e9c480e6bd5917be5034351135ae7ac607a33
Derek Vawdrey <flyingpiechicken@gmail.com>
2018-03-01 11:58:29 -0700

    added collectTweets method

--- src/chat/model/CTECTwitter.java
+++ src/chat/model/CTECTwitter.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import chat.controller.ChatbotController;
+import twitter4j.Paging;
+import twitter4j.ResponseList;
 import twitter4j.Status;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
@@ -38,6 +40,38 @@ public class CTECTwitter
 			appController.handleErrors(tweetError);
 		}catch(Exception otherError){
 			appController.handleErrors(otherError);
+		}
+	}
+	
+	public String getMostCommonWord(String username) {
+		String mostCommon = "";
+		
+		collectTweets(username);
+		
+		return mostCommon;
+	}
+	
+	private void collectTweets(String username) {
+		searchedTweets.clear();
+		tweetedWords.clear();
+		Paging statusPage = new Paging(1,100);
+		int page = 1;
+		long lastID = Long.MAX_VALUE;
+		
+		while(page <= 10) {
+			statusPage.setPage(page);
+			try {
+				ResponseList<Status> listedTweets = chatbotTwitter.getUserTimeline(username, statusPage);
+				for(Status current : listedTweets) {
+					if(current.getId() < lastID) {
+						searchedTweets.add(current);
+						lastID = current.getId();
+					}
+				}
+			}catch(TwitterException searchTweetError) {
+				appController.handleErrors(searchTweetError);
+			}
+			page++;
 		}
 	}
 	
